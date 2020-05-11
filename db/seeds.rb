95147f7a90ce0d94c7b8f963bc74cd3ea55d6f2d
Morgan Reilly <G00303598@gmit.ie>
2020-05-11 21:59:43 +0000

    index page built, not yet tested, having some issues trying to run the application...

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-produce = [{:product_name => 'Bananas', :quantity => '40', :next_delivery => '12-May-2020'},
+produce = [
+        {:product_name => 'Bananas', :quantity => '40', :next_delivery => '12-May-2020'},
     	{:product_name => 'Grapes', :quantity => '30', :next_delivery => '11-May-2020'},
     	{:product_name => 'Potatoes', :quantity => '100', :next_delivery => '13-May-2020'},
       	{:product_name => 'Celeriac', :quantity => '0', :next_delivery => '20-May-2020'},
